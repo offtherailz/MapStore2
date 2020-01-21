2bbd16ed828f65668ab82505a774f2f517b23d2a
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2020-01-21 16:24:23 +0100

    WIP about replacing async calls

--- web/client/jsapi/__tests__/MapStore2-test.js
+++ web/client/jsapi/__tests__/MapStore2-test.js
@@ -15,20 +15,44 @@ const { CHANGE_BROWSER_PROPERTIES } = require('../../actions/browser');
 
 const { LOCAL_CONFIG_LOADED } = require('../../actions/localConfig');
 
+const axios = require("../../libs/ajax");
+const MockAdapter = require("axios-mock-adapter");
 
 const expect = require('expect');
 const MapStore2 = require('../MapStore2');
 
 const testConfig = {
+    enUs: 'base/web/client/translations/data.en-US',
+    localConfig: 'base/web/client/test-resources/localConfig.json',
     versionURL: 'base/web/client/test-resources/version.txt',
     configUrl: 'base/web/client/test-resources/geostore/data/1#',
     originalUrl: 'base/web/client/test-resources/geostore/extjs/search/category/MAP/1.json#'
 };
+const testData = {
+    enUs: require('json-loader!../../translations/data.en-US'),
+    localConfig: require('../../test-resources/localConfig.json'),
+    versionURL: "TEST",
+    configUrl: require('json-loader!../../test-resources/geostore/data/1'),
+    originalUrl: require('../../test-resources/geostore/extjs/search/category/MAP/1.json')
+};
+
+
 describe('MapStore2 API', () => {
+    let mockAxios;
     beforeEach((done) => {
         document.body.innerHTML = '<div id="container"></div>';
         ConfigUtils.setLocalConfigurationFile('base/web/client/test-resources/localConfig.json');
         ConfigUtils.setConfigProp('translationsPath', "base/web/client/translations");
+        mockAxios = new MockAdapter(axios);
+        mockAxios.onGet(testConfig.versionURL).reply([200, testData.versionURL]);
+        mockAxios.onGet(testConfig.configUrl).reply([200, testData.configUrl]);
+        mockAxios.onGet(testConfig.originalUrl).reply([200, testData.originalUrl]);
+        mockAxios.onGet(testConfig.localConfig).reply([200], testData.localConfig);
+        mockAxios.onGet().reply((a) => {
+            debugger;
+            console.log(a);
+            return [200];
+        });
         setTimeout(done);
     });
     afterEach((done) => {
@@ -36,6 +60,7 @@ describe('MapStore2 API', () => {
         ConfigUtils.setLocalConfigurationFile('localConfig.json');
         ConfigUtils.setConfigProp('translationsPath', "translations");
         document.body.innerHTML = '';
+        mockAxios.restore();
         setTimeout(done);
     });
     it('MapStore2 rendering with defaults', (done) => {
